a20980022be5c60a92c2a6207a0aa5beaca444c8
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-02-02 15:11:29 +0100

    basic sysinfo output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::process::Command;
+use std::time::SystemTime;
+use sysinfo::{Cpu, System};
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new();
+    let mut n = 0;
+    let mut timestamp_nanos: u128;
+
+    while n < 10 {
+        sys.refresh_cpu();
+
+        let duration_since_epoch = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        timestamp_nanos = duration_since_epoch.as_nanos();
+
+        print!("{},", timestamp_nanos);
+        for cpu in sys.cpus() {
+            print!(" {}%,", cpu.cpu_usage());
+        }
+        print!("\n");
+
+        let mut wait = Command::new("sleep").arg("1").spawn().unwrap();
+        let _result = wait.wait().unwrap();
+
+        n += 1
+    }
 }
