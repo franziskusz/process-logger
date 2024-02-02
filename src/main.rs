4e0723c9956d8cb5b33592c4396a1b6ce8d44134
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-02-02 15:33:48 +0100

    add memory data and outcommented temperature data

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::process::Command;
 use std::time::SystemTime;
-use sysinfo::{Cpu, System};
+use sysinfo::System;
 
 fn main() {
     let mut sys = System::new();
     let mut n = 0;
     let mut timestamp_nanos: u128;
 
-    while n < 10 {
+    print!("timestamp, cpu1, cpu2, cpu3, cpu4, used memory, used swap\n");
+
+    while n < 100 {
         sys.refresh_cpu();
+        sys.refresh_memory();
+        //let components = Components::new_with_refreshed_list();
 
         let duration_since_epoch = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
@@ -19,6 +23,13 @@ fn main() {
         for cpu in sys.cpus() {
             print!(" {}%,", cpu.cpu_usage());
         }
+
+        print!(" {},", sys.used_memory());
+        print! {" {}", sys.used_swap()};
+
+        //for component in &components {
+        //    print!("{component:?}");
+        //}
         print!("\n");
 
         let mut wait = Command::new("sleep").arg("1").spawn().unwrap();
